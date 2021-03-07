5ea8520e80febd717bd2e90a6480b34149de6db0
Ildiko Rigo <ildiko.bcb@gmail.com>
2021-03-07 13:45:46 +0100

    Clear Search Field works onClick

--- src/components/SearchOverlay.js
+++ src/components/SearchOverlay.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useSelector, useDispatch  } from 'react-redux';
+import { setSearchTerm } from '../actions/AppActions';
 
-import Backdrop from '@material-ui/core/Backdrop';
+import Typography from '@material-ui/core/Typography';
 
 // import GenericCard from './GenericCard';
 // import { setCategory, setActivePage } from '../actions/AppActions';
@@ -9,17 +10,32 @@ import Backdrop from '@material-ui/core/Backdrop';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-    backdrop: {
-        zIndex: theme.zIndex.drawer + 1,
+    overlay: {
+        position: 'absolute',
+        width: '100%',
+        height: 'calc(100vh - 64px)',
+        textAlign: 'right',
+        opacity: '95%',
+        backgroundColor: 'grey',
+        zIndex: 1500,
         color: '#fff',
     },
+    hideOverlay:{
+        display: 'none'
+    },
+    clear: {
+        padding: '16px',
+    }
 }));
 
 export const SearchOverlay = () => {
     const classes = useStyles();
     const searchTerm = useSelector( state => state.searchTerm );
+    const dispatch = useDispatch();
 
-    return (<Backdrop className={classes.backdrop} open={ searchTerm !== '' }>
-                HELLO!
-            </Backdrop>)
+    return (<div className={`${classes.overlay} ${searchTerm || classes.hideOverlay}`}>
+                <Typography className={classes.clear} noWrap onClick={() => dispatch(setSearchTerm(''))}>
+                    CLEAR SEARCH FIELD
+                </Typography>
+            </div>)
 };
